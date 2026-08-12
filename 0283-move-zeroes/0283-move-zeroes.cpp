0fea84abae5e614ae53a348b735c54b7a6cf4425
utkarshkhatri007-git <utkarshkhatri007@gmail.com>
2026-08-12 18:58:41 +0530

    Time: 422 ms (5.03%), Space: 23.7 MB (99.01%) - LeetHub

---
+++ 0283-move-zeroes/0283-move-zeroes.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    void moveZeroes(vector<int>& nums) 
+    {
+        int n = nums.size();
+
+        for(int i = n - 1; i >= 0; i--)
+        {
+            for(int j = 0; j < i; j++)
+            {
+                if(nums[j] == 0)
+                {
+                    swap(nums[j], nums[j + 1]);
+                }
+            }
+        }
+    }
+};
